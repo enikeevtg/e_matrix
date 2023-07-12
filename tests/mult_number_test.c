afb6747f09b74b020af70fcf47be8f63e8f8195f
Tagir Enikeev <enikeev.tg@gmail.com>
2023-07-13 02:17:52 +0300

    mult_number_test expanded

--- tests/mult_number_test.c
+++ tests/mult_number_test.c
@@ -237,6 +237,64 @@ START_TEST(pos_mult_sqr_matrix_OK_4) {
 }
 END_TEST
 
+START_TEST(pos_mult_sqr_matrix_OK_5) {
+  // Arrange
+  // test_matrix
+  int rows = 11;
+  int cols = 11;
+  matrix_t test_matrix = {0};
+  matrix_filling(rows, cols, &test_matrix, POS_SIMPLE_SEQUENCE_X2);
+  double scalar = 0.5f;
+  // result matrix
+  matrix_t result_matrix = {0};
+  // reference matrix
+  int ref_rows = 11;
+  int ref_cols = 11;
+  matrix_t ref_matrix = {0};
+  matrix_filling(ref_rows, ref_cols, &ref_matrix, POS_SIMPLE_SEQUENCE);
+
+  // Act
+  e_mult_number(&test_matrix, scalar, &result_matrix);
+
+  // Assert
+  int match = e_eq_matrix(&result_matrix, &ref_matrix);
+  ck_assert_int_eq(match, SUCCESS);
+
+  e_remove_matrix(&test_matrix);
+  e_remove_matrix(&result_matrix);
+  e_remove_matrix(&ref_matrix);
+}
+END_TEST
+
+START_TEST(pos_mult_sqr_matrix_OK_6) {
+  // Arrange
+  // test_matrix
+  int rows = 11;
+  int cols = 11;
+  matrix_t test_matrix = {0};
+  matrix_filling(rows, cols, &test_matrix, NEG_SIMPLE_SEQUENCE_X2);
+  double scalar = -0.5f;
+  // result matrix
+  matrix_t result_matrix = {0};
+  // reference matrix
+  int ref_rows = 11;
+  int ref_cols = 11;
+  matrix_t ref_matrix = {0};
+  matrix_filling(ref_rows, ref_cols, &ref_matrix, POS_SIMPLE_SEQUENCE);
+
+  // Act
+  e_mult_number(&test_matrix, scalar, &result_matrix);
+
+  // Assert
+  int match = e_eq_matrix(&result_matrix, &ref_matrix);
+  ck_assert_int_eq(match, SUCCESS);
+
+  e_remove_matrix(&test_matrix);
+  e_remove_matrix(&result_matrix);
+  e_remove_matrix(&ref_matrix);
+}
+END_TEST
+
 START_TEST(pos_mult_rect_matrix_OK_1) {
   // Arrange
   // test_matrix
@@ -339,6 +397,64 @@ START_TEST(pos_mult_rect_matrix_OK_4) {
   int ref_cols = 11;
   matrix_t ref_matrix = {0};
   matrix_filling(ref_rows, ref_cols, &ref_matrix, POS_SIMPLE_SEQUENCE_X2);
+
+  // Act
+  e_mult_number(&test_matrix, scalar, &result_matrix);
+
+  // Assert
+  int match = e_eq_matrix(&result_matrix, &ref_matrix);
+  ck_assert_int_eq(match, SUCCESS);
+
+  e_remove_matrix(&test_matrix);
+  e_remove_matrix(&result_matrix);
+  e_remove_matrix(&ref_matrix);
+}
+END_TEST
+
+START_TEST(pos_mult_rect_matrix_OK_5) {
+  // Arrange
+  // test_matrix
+  int rows = 8;
+  int cols = 11;
+  matrix_t test_matrix = {0};
+  matrix_filling(rows, cols, &test_matrix, POS_SIMPLE_SEQUENCE_X2);
+  double scalar = 0.5f;
+  // result matrix
+  matrix_t result_matrix = {0};
+  // reference matrix
+  int ref_rows = 8;
+  int ref_cols = 11;
+  matrix_t ref_matrix = {0};
+  matrix_filling(ref_rows, ref_cols, &ref_matrix, POS_SIMPLE_SEQUENCE);
+
+  // Act
+  e_mult_number(&test_matrix, scalar, &result_matrix);
+
+  // Assert
+  int match = e_eq_matrix(&result_matrix, &ref_matrix);
+  ck_assert_int_eq(match, SUCCESS);
+
+  e_remove_matrix(&test_matrix);
+  e_remove_matrix(&result_matrix);
+  e_remove_matrix(&ref_matrix);
+}
+END_TEST
+
+START_TEST(pos_mult_rect_matrix_OK_6) {
+  // Arrange
+  // test_matrix
+  int rows = 8;
+  int cols = 11;
+  matrix_t test_matrix = {0};
+  matrix_filling(rows, cols, &test_matrix, NEG_SIMPLE_SEQUENCE_X2);
+  double scalar = -0.5f;
+  // result matrix
+  matrix_t result_matrix = {0};
+  // reference matrix
+  int ref_rows = 8;
+  int ref_cols = 11;
+  matrix_t ref_matrix = {0};
+  matrix_filling(ref_rows, ref_cols, &ref_matrix, POS_SIMPLE_SEQUENCE);
 
   // Act
   e_mult_number(&test_matrix, scalar, &result_matrix);
@@ -471,6 +587,64 @@ START_TEST(neg_mult_sqr_matrix_OK_4) {
 }
 END_TEST
 
+START_TEST(neg_mult_sqr_matrix_OK_5) {
+  // Arrange
+  // test_matrix
+  int rows = 11;
+  int cols = 11;
+  matrix_t test_matrix = {0};
+  matrix_filling(rows, cols, &test_matrix, POS_SIMPLE_SEQUENCE_X2);
+  double scalar = -0.5f;
+  // result matrix
+  matrix_t result_matrix = {0};
+  // reference matrix
+  int ref_rows = 11;
+  int ref_cols = 11;
+  matrix_t ref_matrix = {0};
+  matrix_filling(ref_rows, ref_cols, &ref_matrix, NEG_SIMPLE_SEQUENCE);
+
+  // Act
+  e_mult_number(&test_matrix, scalar, &result_matrix);
+
+  // Assert
+  int match = e_eq_matrix(&result_matrix, &ref_matrix);
+  ck_assert_int_eq(match, SUCCESS);
+
+  e_remove_matrix(&test_matrix);
+  e_remove_matrix(&result_matrix);
+  e_remove_matrix(&ref_matrix);
+}
+END_TEST
+
+START_TEST(neg_mult_sqr_matrix_OK_6) {
+  // Arrange
+  // test_matrix
+  int rows = 11;
+  int cols = 11;
+  matrix_t test_matrix = {0};
+  matrix_filling(rows, cols, &test_matrix, NEG_SIMPLE_SEQUENCE_X2);
+  double scalar = 0.5f;
+  // result matrix
+  matrix_t result_matrix = {0};
+  // reference matrix
+  int ref_rows = 11;
+  int ref_cols = 11;
+  matrix_t ref_matrix = {0};
+  matrix_filling(ref_rows, ref_cols, &ref_matrix, NEG_SIMPLE_SEQUENCE);
+
+  // Act
+  e_mult_number(&test_matrix, scalar, &result_matrix);
+
+  // Assert
+  int match = e_eq_matrix(&result_matrix, &ref_matrix);
+  ck_assert_int_eq(match, SUCCESS);
+
+  e_remove_matrix(&test_matrix);
+  e_remove_matrix(&result_matrix);
+  e_remove_matrix(&ref_matrix);
+}
+END_TEST
+
 START_TEST(neg_mult_rect_matrix_OK_1) {
   // Arrange
   // test_matrix
@@ -587,6 +761,64 @@ START_TEST(neg_mult_rect_matrix_OK_4) {
 }
 END_TEST
 
+START_TEST(neg_mult_rect_matrix_OK_5) {
+  // Arrange
+  // test_matrix
+  int rows = 8;
+  int cols = 11;
+  matrix_t test_matrix = {0};
+  matrix_filling(rows, cols, &test_matrix, POS_SIMPLE_SEQUENCE_X2);
+  double scalar = -0.5f;
+  // result matrix
+  matrix_t result_matrix = {0};
+  // reference matrix
+  int ref_rows = 8;
+  int ref_cols = 11;
+  matrix_t ref_matrix = {0};
+  matrix_filling(ref_rows, ref_cols, &ref_matrix, NEG_SIMPLE_SEQUENCE);
+
+  // Act
+  e_mult_number(&test_matrix, scalar, &result_matrix);
+
+  // Assert
+  int match = e_eq_matrix(&result_matrix, &ref_matrix);
+  ck_assert_int_eq(match, SUCCESS);
+
+  e_remove_matrix(&test_matrix);
+  e_remove_matrix(&result_matrix);
+  e_remove_matrix(&ref_matrix);
+}
+END_TEST
+
+START_TEST(neg_mult_rect_matrix_OK_6) {
+  // Arrange
+  // test_matrix
+  int rows = 8;
+  int cols = 11;
+  matrix_t test_matrix = {0};
+  matrix_filling(rows, cols, &test_matrix, NEG_SIMPLE_SEQUENCE_X2);
+  double scalar = 0.5f;
+  // result matrix
+  matrix_t result_matrix = {0};
+  // reference matrix
+  int ref_rows = 8;
+  int ref_cols = 11;
+  matrix_t ref_matrix = {0};
+  matrix_filling(ref_rows, ref_cols, &ref_matrix, NEG_SIMPLE_SEQUENCE);
+
+  // Act
+  e_mult_number(&test_matrix, scalar, &result_matrix);
+
+  // Assert
+  int match = e_eq_matrix(&result_matrix, &ref_matrix);
+  ck_assert_int_eq(match, SUCCESS);
+
+  e_remove_matrix(&test_matrix);
+  e_remove_matrix(&result_matrix);
+  e_remove_matrix(&ref_matrix);
+}
+END_TEST
+
 /*===========================nill result tests============================*/
 
 START_TEST(nill_mult_sqr_matrix_OK) {
@@ -666,10 +898,14 @@ Suite* mult_number(void) {
   tcase_add_test(mult_number_positive, pos_mult_sqr_matrix_OK_2);
   tcase_add_test(mult_number_positive, pos_mult_sqr_matrix_OK_3);
   tcase_add_test(mult_number_positive, pos_mult_sqr_matrix_OK_4);
+  tcase_add_test(mult_number_positive, pos_mult_sqr_matrix_OK_5);
+  tcase_add_test(mult_number_positive, pos_mult_sqr_matrix_OK_6);
   tcase_add_test(mult_number_positive, pos_mult_rect_matrix_OK_1);
   tcase_add_test(mult_number_positive, pos_mult_rect_matrix_OK_2);
   tcase_add_test(mult_number_positive, pos_mult_rect_matrix_OK_3);
   tcase_add_test(mult_number_positive, pos_mult_rect_matrix_OK_4);
+  tcase_add_test(mult_number_positive, pos_mult_rect_matrix_OK_5);
+  tcase_add_test(mult_number_positive, pos_mult_rect_matrix_OK_6);
   suite_add_tcase(s, mult_number_positive);
 
   TCase* mult_number_negative =
@@ -678,10 +914,14 @@ Suite* mult_number(void) {
   tcase_add_test(mult_number_negative, neg_mult_sqr_matrix_OK_2);
   tcase_add_test(mult_number_negative, neg_mult_sqr_matrix_OK_3);
   tcase_add_test(mult_number_negative, neg_mult_sqr_matrix_OK_4);
+  tcase_add_test(mult_number_negative, neg_mult_sqr_matrix_OK_5);
+  tcase_add_test(mult_number_negative, neg_mult_sqr_matrix_OK_6);
   tcase_add_test(mult_number_negative, neg_mult_rect_matrix_OK_1);
   tcase_add_test(mult_number_negative, neg_mult_rect_matrix_OK_2);
   tcase_add_test(mult_number_negative, neg_mult_rect_matrix_OK_3);
   tcase_add_test(mult_number_negative, neg_mult_rect_matrix_OK_4);
+  tcase_add_test(mult_number_negative, neg_mult_rect_matrix_OK_5);
+  tcase_add_test(mult_number_negative, neg_mult_rect_matrix_OK_6);
   suite_add_tcase(s, mult_number_negative);
 
   TCase* mult_number_0 = tcase_create("mult_number result == 0 function tests");
